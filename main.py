3b1fbad3bf6aca4602d2b23b79562eb614a80a14
JujubaOhime <ameliadag96@gmail.com>
2020-11-29 17:44:23 -0300

    começando a fazer o CSR

---
+++ main.py
@@ -0,0 +1,27 @@
+def multiplica(vaa, vja, via, outroVetor):
+    pass
+
+def transposta(vaa, vja, via, n_linha, n_coluna):
+    new_vaa = []
+    new_vja = []
+    new_via = []
+    posicao = []
+    for i in vaa:
+        
+
+
+
+    
+
+b = [7, 2.5, 6.002]
+
+vaa = [10, -7, 2.5, 5, 6.002]
+vja = [1, 2, 2, 3, 3]
+via = [1, 3, 5]
+
+x0 = [x1, x2, x3]
+
+
+r0 = b
+
+

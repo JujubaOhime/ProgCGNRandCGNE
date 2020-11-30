384a5f528e88029d94132680e2bd6316ad4df4bc
JujubaOhime <ameliadag96@gmail.com>
2020-11-29 21:25:40 -0300

    feito subtração

--- main.py
+++ main.py
@@ -88,6 +88,43 @@ class CSR:
         
         res = CSR(new_vaa, new_vja, new_via, self.linhas, self.colunas)
         return(res)
+
+    def subtracao(self, matriz):
+        new_vaa = []
+        new_vja = []
+        new_via = []
+        posicoes = []
+        posicoes2 = []
+
+        for i in range(len(self.vaa)):
+            posicoes.append(self.acha_linha_coluna_de_elemento(i))
+        
+        for i in range(len(matriz.vaa)):
+            posicoes2.append(matriz.acha_linha_coluna_de_elemento(i))
+
+        somatorio = 0
+        for i in range(self.linhas):
+            menor = 0
+            for j in range(self.colunas):
+
+                if ([i, j] in posicoes) or ([i,j] in posicoes2):
+                    soma = 0
+
+                    if([i,j] in posicoes):
+                        soma = soma + self.vaa[posicoes.index([i, j])]
+
+                    if([i,j] in posicoes2):
+                        soma = soma - matriz.vaa[posicoes2.index([i, j])]
+                    
+                    new_vaa.append(soma)
+                    new_vja.append(j)
+                    if (menor == 0):
+                        new_via.append(somatorio)
+                    somatorio = somatorio + 1
+                    menor = menor + 1
+        
+        res = CSR(new_vaa, new_vja, new_via, self.linhas, self.colunas)
+        return(res)
     
 
 '''def acha_linha_coluna_de_elemento(posicao,vaa, vja, via):
@@ -186,5 +223,6 @@ b = CSR([1, 7, 3, 5, 9], [0,2,1,2,1], [0, 2, 4], 3, 3)
 #print(matriz)
 #print(matriz.transposta())
 print(a.soma(b))
+print(a.subtracao(b))
 
 
